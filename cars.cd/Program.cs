63b5745155757304937bef6c0313dacf2aa779ed
FarikGang <112854193+FarikGang@users.noreply.github.com>
2023-09-20 01:34:02 +0500

    car 1

---
+++ cars.cd/Program.cs
@@ -0,0 +1,30 @@
+﻿``public class Cars {
+    int model;
+    int price;
+    int color;
+
+    public Cars( string model, int price, string color) {
+        this.model = model;
+        this.price = price;
+        this.color = color;
+    }
+
+    public int firstModel() {
+        return model;
+    }
+    public int firstPrice() {
+        return price;
+    }
+    public int firstColor() {
+        return color;
+    }
+}
+
+
+public class Car {
+    public static void Main() {
+        Cars firstHero = new Cars(BMW, 20000, black);
+        Console.WriteLine("Марка" + firstModel() +"Цена" + firstPrice() + "Цвет" + firstColor());
+    }
+
+}
